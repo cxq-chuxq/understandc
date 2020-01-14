3d0c9ea5f681721d4c63937ec5c20461a7fa5a66
edz <edz@chuxq.local>
2020-01-14 10:01:12 +0800

    作业1-1

---
+++ HelloC/1-1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>  
+int main(void){  
+   int a, b;  
+   puts("请输入两个整数");  
+   printf("整数1:");  
+   scanf("%d", &a);  
+   printf("整数2:");  
+   scanf("%d", &b);  
+   printf("整数1减去整数2的结果是%d\n", a - b);  
+   return 0;  
+} 
